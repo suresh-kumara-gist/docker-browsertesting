a1f9a43ae4756d75a854f27f20d075a27a3139a9
alberto56 <albert@mediatribe.net>
2017-11-09 16:07:43 -0500

    Update name of Phantom JS npm library

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 # See
 #  http://thejsguy.com/2015/02/28/end-to-end-testing-with-phantomsjs-and-casperjs.html
-FROM node
+# See https://github.com/Medium/phantomjs/issues/707 on why we are using
+# node 7
+FROM node:7
 
 RUN mkdir -p /app/code
 
-RUN cd /app && npm install phantomjs -g
+RUN cd /app && npm install phantomjs-prebuilt -g
 RUN cd /app && npm install casperjs -g
 
 WORKDIR /app
---
+++ scripts/rebuild.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Rebuild script
+# This is meant to be run on a regular basis to make sure everything works with
+# the latest version of scripts.
+
+set -e
+
+CREDENTIALS="$HOME/.dcycle-docker-credentials.sh"
+
+if [ ! -f "$CREDENTIALS" ]; then
+  echo "Please create $CREDENTIALS and add to it:"
+  echo "DOCKERHUBUSER=xxx"
+  echo "DOCKERHUBPASS=xxx"
+  exit;
+else
+  source "$CREDENTIALS";
+fi
+
+DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
+VERSION='1.1'
+
+# Start by getting the latest version of the official drupal image
+docker pull node
+# Rebuild the entire thing
+docker build --no-cache -t dcycle/browsertesting:"$VERSION" .
+docker build -t dcycle/browsertesting:"$VERSION".$DATE .
+docker build -t dcycle/browsertesting:latest .
+docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
+docker push dcycle/browsertesting:"$VERSION"
+docker push dcycle/browsertesting:"$VERSION"."$DATE"
+docker push dcycle/browsertesting:latest
