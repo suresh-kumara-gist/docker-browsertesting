e5bf18d4a7e10230c267414e69ee7c49cc8848ae
Albert Albala <albert@mediatribe.net>
2022-04-21 09:05:55 -0400

    Compatibility with ARM infrastructure hosts, remove libappindicator1 which is not locatable and cross fingers that that does not break anything (#10)

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -18,20 +18,19 @@ fi
 
 ./ci.sh
 
+PROJECT=browsertesting
 DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
 MAJORVERSION='3'
 VERSION='3.0'
 
-# Start by getting the latest version of the official drupal image
-docker pull node
-# Rebuild the entire thing
-docker build --no-cache -t dcycle/browsertesting:"$VERSION" .
-docker build -t dcycle/browsertesting:"$MAJORVERSION" .
-docker build -t dcycle/browsertesting:"$MAJORVERSION".$DATE .
-docker build -t dcycle/browsertesting:"$VERSION".$DATE .
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
 docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
-docker push dcycle/browsertesting:"$VERSION"
-docker push dcycle/browsertesting:"$MAJORVERSION"
-docker push dcycle/browsertesting:"$VERSION"."$DATE"
-docker push dcycle/browsertesting:"$MAJORVERSION"."$DATE"
-# No longer using the latest tag, use the major version tag instead.
+
+docker pull node
+
+docker buildx build -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
